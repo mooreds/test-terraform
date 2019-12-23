6a1ffafcd93d47fd79ce36ca52c90f7893520ee1
Dan Moore <github@mooreds.com>
2019-12-23 14:20:33 -0700

    first checkin

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~> 0.12"
+
+  backend "s3" {
+    bucket         = "mooreds-tf-state-jenkinspoc"
+    key            = "s3/terraform.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.13"
+  profile = "jenkinspoc"
+  region  = "us-west-2"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-test-mooreds"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
